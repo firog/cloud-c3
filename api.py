f78deadeab3f311baab02e093ff74ad7cf3bec57
firog <olofmarkstedt@gmail.com>
2016-10-11 18:51:21 +0000

    hej

--- api.py
+++ api.py
@@ -1,9 +1,14 @@
-import tasks
+from tasks import count_words
 from flask import Flask
-import json
-
 app = Flask(__name__)
 
-@app.route("/twitter/api")
+@app.route('/index')
+def index():
+        return "Count tweets"
+
+@app.route('/count_tweets/api/v1.0/count', methods=['GET'])
 def count_tweets():
-	return count_tweets.delay()	
+        r = count_words.delay("/home/ubuntu/cloud-c3/proj/tweets/05cb5036-2170-401b-947d-68f9191b21c6")	
+        return r.get() 
+if __name__ == '__main__':
+        app.run(host='0.0.0.0',debug=True)
