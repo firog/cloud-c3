ce49c80b5f5c2acf46f268ce8696f09ad3a64c07
firog <olofmarkstedt@gmail.com>
2016-10-13 21:02:27 +0000

    hej

--- api.py
+++ api.py
@@ -1,14 +1,21 @@
 from tasks import count_words
-from flask import Flask
+from flask import Flask, render_template, send_file, make_response
+import matplotlib.pyplot as plt
+import plotly.plotly as py
+from io import BytesIO
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import numpy as np
+import prettyplotlib as ppl
 app = Flask(__name__)
 
 @app.route('/index')
 def index():
-        return "Count tweets"
+        return "Count tweets with /count_tweets/api/v1.0/count"
 
 @app.route('/count_tweets/api/v1.0/count', methods=['GET'])
 def count_tweets():
-        r = count_words.delay("/home/ubuntu/cloud-c3/proj/tweets/05cb5036-2170-401b-947d-68f9191b21c6")	
-        return r.get() 
+        r = count_words.delay()
+        return r.get()
+
 if __name__ == '__main__':
         app.run(host='0.0.0.0',debug=True)
--- tasks.py
+++ tasks.py
@@ -1,27 +1,39 @@
 from celery import Celery
 import os
 import json
+import requests
 
 app = Celery('tasks', broker='amqp://guest@localhost//', backend='rpc://')
 
 @app.task
-def count_words(file):
-	words = ["hen","han","hon","den","det","denna","denne"]
-        dic = {}
-        for i in words:
-                dic[i]=0
-	for filename in os.listdir("/home/ubuntu/cloud-c3/proj/tweets"):
-                f = open(file,"r")
-                for line in f:
-                        if line.startswith("\n"):
-                                continue
-                        else:
-                                data = json.loads(line)
-                                if not data['retweeted']:
-                                        text = data['text']
-                                        content = text.split()
-                                        for match in words:
-                                                if match in content:
-                                                        dic[match] += 1
-        return  json.dumps(dic)
+def fetch_tweets():
+    save_path = "/home/ubuntu/cloud-c3/proj/tweets"
+    r = requests.get("http://130.238.29.253:8080/swift/v1/tweets")
+    for i in r.content.split():
+        if not os.path.isfile("%s/%s" % (save_path, i)):
+            c = requests.get("http://130.238.29.253:8080/swift/v1/tweets/%s" % i)
+            nf = open(os.path.join(save_path, i),"w")
+            nf.write(c.content)
+        
+@app.task
+def count_words():
+    words = ["hen","han","hon","den","det","denna","denne"]
+    dic = {}
+    for i in words:
+        dic[i] = 0
+    for filename in os.listdir("/home/ubuntu/cloud-c3/proj/tweets"):
+        f = open("/home/ubuntu/cloud-c3/proj/tweets/%s" % filename,"r")
+        for line in f:
+            if line.startswith("\n"):
+                continue
+            else:
+                data = json.loads(line)
+                if not data['retweeted']:
+                    text = data['text']
+                    content = text.split()
+                    for match in words:
+                        if match in content:
+                            dic[match] += 1
+       # f.close()
+    return  json.dumps(dic)
 
