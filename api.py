f8960004b1e09413716882be1b478e71d8e79d7f
firog <olofmarkstedt@gmail.com>
2016-10-14 16:33:32 +0000

    hej

--- api.py
+++ api.py
@@ -1,11 +1,5 @@
-from tasks import count_words
+from tasks import count_words, count_them
 from flask import Flask, render_template, send_file, make_response
-import matplotlib.pyplot as plt
-import plotly.plotly as py
-from io import BytesIO
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-import numpy as np
-import prettyplotlib as ppl
 app = Flask(__name__)
 
 @app.route('/index')
@@ -17,5 +11,9 @@ def count_tweets():
         r = count_words.delay()
         return r.get()
 
+@app.route('/count_tweets/api/v1.0/count', methods=['GET'])
+def counter():
+        t = count_them()
+        return t
 if __name__ == '__main__':
         app.run(host='0.0.0.0',debug=True)
--- tasks.py
+++ tasks.py
@@ -19,8 +19,10 @@ def fetch_tweets():
 def count_words():
     words = ["hen","han","hon","den","det","denna","denne"]
     dic = {}
+  #  num_tweets = "tweets"
     for i in words:
         dic[i] = 0
+ #   dic[num_tweets] = 0
     for filename in os.listdir("/home/ubuntu/cloud-c3/proj/tweets"):
         f = open("/home/ubuntu/cloud-c3/proj/tweets/%s" % filename,"r")
         for line in f:
@@ -28,12 +30,23 @@ def count_words():
                 continue
             else:
                 data = json.loads(line)
+#                dic[num_tweets] += 1
                 if not data['retweeted']:
                     text = data['text']
                     content = text.split()
                     for match in words:
                         if match in content:
                             dic[match] += 1
-       # f.close()
     return  json.dumps(dic)
 
+@app.task
+def count_them():
+    count = 0
+    for filename in os.listdir("/home/ubuntu/cloud-c3/proj/tweets"):
+        f = open("/home/ubuntu/cloud-c3/proj/tweets/%s" % filename,"r")
+        for line in f:
+            if line.startswith("\n"):
+                continue
+            else:
+                count += 1
+    return count
